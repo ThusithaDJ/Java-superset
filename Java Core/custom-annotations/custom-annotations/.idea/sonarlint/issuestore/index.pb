aa293d8a2e40ed0682c16e8d9418ccbf913fd7c2
Thusitha Jayasundara <thusitha.jayasundara@gmail.com>
2023-11-06 22:04:24 +0530

    Custom annotation testing

---
+++ Java Core/custom-annotations/custom-annotations/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+m
+=src/main/java/org/example/myannotations/MyFistAnnotation.java,4\8\48004dd30b84247428e25ded516b85f416dd71e0
+S
+#src/main/java/org/example/Main.java,0\b\0ba1b9c7a4878802f938e331cc2d8a042087ec6b
+?
+settings.gradle,0\5\05efc8b1657769a27696d478ded1e95f38737233
+X
+(gradle/wrapper/gradle-wrapper.properties,f\b\fbe448ebfc3eb2d4e308f6b8b043666f5b57235e
+F
+custom-annotations.iml,1\c\1c9b0a932ecccb0a64cd0898f4bda89ee93b8eb7
---
+++ Java Core/custom-annotations/custom-annotations/src/main/java/org/example/Main.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import org.example.myannotations.MyFistAnnotation;
+
+import java.lang.reflect.Method;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+
+        /**
+         * Execute only the instance methods decorated with  annotation
+         */
+        Main main = new Main();
+        for (Method m : main.getClass().getMethods()) {
+            if (m.isAnnotationPresent(MyFistAnnotation.class)) {
+                try {
+                    m.invoke(main);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+        /**
+         * This will trigger both static and instance method. Because, Main.class.getDeclaredMethods() returns all the methods.
+         * So when m.invoke(new Main()) method call, it'll first execute the static method and then execute the instance method.
+         */
+        for(Method m : Main.class.getDeclaredMethods()) {
+            if (m.isAnnotationPresent(MyFistAnnotation.class)) {
+                try {
+                    m.invoke(new Main());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @MyFistAnnotation
+    public void shouldTriggeredFromAnnotation() {
+        System.out.println("Triggered from annotation");
+    }
+
+    @MyFistAnnotation
+    static void staticMethodShouldTriggeredFromAnnotation() {
+        System.out.println("Static method triggered from annotation");
+    }
+}
